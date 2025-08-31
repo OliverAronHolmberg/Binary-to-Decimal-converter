cedf1728b474f0f5ac2e7da9493f6d6a82e38db5
Oliver Holmberg <63006762+Oliver1526@users.noreply.github.com>
2025-08-31 12:46:12 +0200

    Refactoring: added function

--- converter.py
+++ converter.py
@@ -1,23 +1,30 @@
+
+def binary_to_decimal_converter(binary_number):
+
+
+    binary_list = list(str(binary_number))
+
+
+    decimal_list = []
+
+    exponent=0
+
+    for number in binary_list.__reversed__():
+        number = int(number)
+        
+        x = number*2**exponent
+        exponent+=1
+        decimal_list.append(x)
+
+
+    decimal_number = 0
+
+    for decimal in decimal_list:
+        decimal_number+=decimal
+
+    return decimal_number
+
 binary_number = int(input("Write a binary number: "))
-
-binary_list = list(str(binary_number))
-
-
-decimal_list = []
-
-exponent=0
-
-for number in binary_list.__reversed__():
-    number = int(number)
-    
-    x = number*2**exponent
-    exponent+=1
-    decimal_list.append(x)
-
-
-decimal_number = 0
-
-for decimal in decimal_list:
-    decimal_number+=decimal
+decimal_number = binary_to_decimal_converter(binary_number)
 
 print(decimal_number)
